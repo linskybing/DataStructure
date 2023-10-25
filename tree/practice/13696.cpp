82faeba09ea5701059f798a858063a3eba53c61b
linskybing <linskybing@gmail.com>
2023-10-25 22:45:46 +0800

    tree

---
+++ tree/practice/13696.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+template <class T> class Tree;
+
+template <class T>
+class Node {
+    friend class tree;
+    private:
+        int number;
+        Node<T>* subNode;
+
+    public: 
+        Node(int n):number(n), nextNode(NULL){}
+
+        void addNode(int n) {
+
+        }
+};
+
+template <class T>
+class Tree {
+    private:
+        Node<T>* nodeList;
+        int totalNode;
+    public:
+        Tree(int total  = 10), totalNode(total) {
+            Node<T>* nodeList = new Node<T>[total+1];
+        }
+
+
+
+};
